715005c4ea6f5a1d0fe5a8d3d5620ca556ea40ab
prajwal971 <prajwalkumar971@gmail.com>
2022-11-30 14:13:29 +0400

    Added category and card-category component.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import {
@@ -7,6 +7,7 @@ import {
     MagnifyingGlassIcon,
     AdjustmentsVerticalIcon
 } from "react-native-heroicons/outline";
+import Categories from '../components/Categories';
 
 const HomeScreen = () => {
     const navigation = useNavigation()
@@ -19,9 +20,8 @@ const HomeScreen = () => {
 
     return (
         <SafeAreaView className="bg-white pt-5">
-            <Text className="text-red-500"></Text>
             {/* Header */}
-            <View className="flex-row pb-3 items-center mx-5 space-x-2">
+            <View className="flex-row pb-3 items-center mx-4 space-x-2">
                 <Image
                     source={{
                         uri: 'https://links.papareact.com/wru',
@@ -30,9 +30,7 @@ const HomeScreen = () => {
                 />
 
                 <View className="flex-1" >
-                    <Text className="font-bold text-gray-400 text-xs">
-                        Deliver Now!
-                    </Text>
+                    <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
                     <Text className="font-bold text-xl">
                         Current Location
                         <ChevronDownIcon size={20} color="#00CC88" />
@@ -43,17 +41,26 @@ const HomeScreen = () => {
             </View>
 
             {/* Search */}
-            <View className="flex-row items-center space-x-2 pb-2 mx-4 px-1">
+            <View className="flex-row items-center space-x-2 pb-2 mx-4">
                 <View className="flex-1 flex-row space-x-2 bg-gray-200 p-3 rounded">
                     <MagnifyingGlassIcon size={20} color="gray" />
                     <TextInput
                         placeholder='Restraunts and cuisines'
                         keyboardType='default'
-                        className='h-5'
                     />
                 </View>
-                <AdjustmentsVerticalIcon size={20} color="#00CC88" />
+                <AdjustmentsVerticalIcon color="#00CC88" />
             </View>
+            <ScrollView
+                className="bg-gray-100"
+                contentContainerStyle={{
+                    paddingBottom:100,
+                }}
+            >
+                {/* categories */}
+                    <Categories/>
+                {/* Featured */}
+            </ScrollView>
 
         </SafeAreaView>
     )
