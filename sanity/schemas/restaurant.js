9ad9b496eaeda59170e658ebb878304aa841665e
prajwal971 <prajwalkumar971@gmail.com>
2022-12-05 14:29:06 +0400

    Added Restaurant schema.

---
+++ sanity/schemas/restaurant.js
@@ -0,0 +1,40 @@
+export default {
+  name: 'restaurant',
+  title: 'Restaurant',
+  type: 'document',
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Restaurant name",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "short_description",
+      type: "string",
+      title: "Short Description",
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "image of the Restaurant",
+    },
+    {
+      name: "lat",
+      type: "number",
+      title: "Latitude of the Restaurant",
+    },
+    {
+      name: "long",
+      type: "number",
+      title: "Longitude of the Restaurant",
+    },
+    {
+      name: "address",
+      type: "string",
+      title: "Restaurant address",
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+}
