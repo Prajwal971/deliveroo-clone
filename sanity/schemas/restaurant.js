99f1e7122487ad71eb616c66355afe5cd7e61ceb
prajwal971 <prajwalkumar971@gmail.com>
2022-12-05 14:38:36 +0400

    Restaurant schema done.

--- sanity/schemas/restaurant.js
+++ sanity/schemas/restaurant.js
@@ -36,5 +36,28 @@ export default {
       title: "Restaurant address",
       validation: (Rule) => Rule.required(),
     },
+    {
+      name: "rating",
+      type: "number",
+      title: "Enter a rating from (1-5 Stars)",
+      validation: (Rule) =>
+        Rule.required()
+          .min(1)
+          .max(5)
+          .error("Please enter a value between 1 and 5")
+    },
+    {
+      name: "type",
+      title: "category",
+      validation: (Rule) => Rule.required(),
+      type: "reference",
+      to: [{ type: "category" }]
+    },
+    {
+      name: "dishes",
+      type: "array",
+      title: "Dishes",
+      of: [{ type: "reference", to: [{ type: "dish" }] }]
+    },
   ],
 }
