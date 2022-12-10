6cc3f3e89f2a75cc7eda21f84fa4070428142adb
prajwal971 <prajwalkumar971@gmail.com>
2022-12-11 00:56:23 +0400

    Dish row component initiated.

--- client/components/DishRow.js
+++ client/components/DishRow.js
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const DishRow = () => {
-  return (
-    <View>
-      <Text>DishRow</Text>
-    </View>
-  )
+const DishRow = ({ id, name, description, price, image }) => {
+    return (
+        <TouchableOpacity>
+            <View>
+                <Text className="text-lg mb-1">{name}</Text>
+                <Text className="text-gray-400">{description}</Text>
+            </View>
+        </TouchableOpacity>
+    )
 }
 
 export default DishRow
--- client/screens/RestaurantScreen.js
+++ client/screens/RestaurantScreen.js
@@ -73,19 +73,19 @@ const RestaurantScreen = () => {
             </View>
 
             <View>
-                <Text className="'px-6 mb-3 font-bold text-xl">
-                    {/* Dishes Row  */}
-                    {dishes.map(dish => (
-                        <DishRow
-                            key={dish._id}
-                            id={dish._id}
-                            name={dish.name}
-                            description={dish.short_description}
-                            price={dish.price}
-                            image={dish.image}
-                        />
-                    ))}
-                </Text>
+                <Text className="'px-6 mb-3 font-bold text-xl">Menu </Text>
+                {/* Dishes Row  */}
+                {dishes.map(dish => (
+                    <DishRow
+                        key={dish._id}
+                        id={dish._id}
+                        name={dish.name}
+                        description={dish.short_description}
+                        price={dish.price}
+                        image={dish.image}
+                    />
+                ))}
+
             </View>
         </ScrollView>
     )
