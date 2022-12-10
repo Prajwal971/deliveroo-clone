e6f7cbf107f11c0655551f7cff9da61d6fa46332
prajwal971 <prajwalkumar971@gmail.com>
2022-12-10 10:48:18 +0400

    Added home page feature from sanity.io and image url fixed for categories.

--- client/components/Categories.js
+++ client/components/Categories.js
@@ -1,20 +1,39 @@
-import { View, Text,ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import CategoryCard from './CategoryCard'
+import sanityClient, { urlFor } from '../sanity'
 
 const Categories = () => {
+
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    sanityClient.fetch(`
+      *[_type == "category"]
+    `).then(data => {
+      setCategories(data)
+    })
+  }, [])
+
   return (
     <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{
-            paddingHorizontal:15,
-            paddingTop:10,
-        }}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        paddingHorizontal: 15,
+        paddingTop: 10,
+      }}
     >
-        {/* Category cards */}
-        <CategoryCard imgUrl = 'https://links.papareact.com/gn7' title='Testing 1'/>
-        
+      {/* Category cards */}
+      {
+        categories.map((category) => (
+          <CategoryCard 
+            key={category._id}
+            imgUrl={urlFor(category.image).width(200).url()}
+            title={category.name} 
+          />
+        ))
+      }
     </ScrollView>
   )
 }
