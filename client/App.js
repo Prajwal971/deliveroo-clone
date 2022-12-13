4b9a8b1efe8c877fd0a010c94d5d293269b39706
prajwal971 <prajwalkumar971@gmail.com>
2022-12-13 16:00:15 +0400

    Basket/cart screen initialised.

--- client/App.js
+++ client/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import RestaurantScreen from './screens/RestaurantScreen';
+
 import { Provider } from "react-redux";
 import { store } from './store';
+
+import BasketScreen from './screens/BasketScreen';
+import HomeScreen from './screens/HomeScreen';
+import RestaurantScreen from './screens/RestaurantScreen';
 
 const Stack = createNativeStackNavigator()
 
@@ -15,6 +18,12 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name='Home' component={HomeScreen} />
           <Stack.Screen name='Restaurant' component={RestaurantScreen} />
+          <Stack.Screen name='Basket' component={BasketScreen} 
+            options={{
+              presentation:'modal',
+              headerShown:false
+            }}
+          />
         </Stack.Navigator>
       </Provider>
     </NavigationContainer>
